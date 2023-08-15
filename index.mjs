149c38af1bf6f573ee14610b1960058b2b66e6c9
Khalid G <kgibson4795@gmail.com>
2023-08-15 09:52:05 -0400

    added all of my headings

--- index.mjs
+++ index.mjs
@@ -1,20 +1,89 @@
 import inquirer from "inquirer";
+import fs from "fs/promises";
 
 
-inquirer
+let {description, tableofcontents, installation, usage, liscense, contributing, tests, questions} = await inquirer
     .prompt([
       {
         type: 'input',
-        name: 'first_name',
-        message: "What's your first name",
+        name: 'description',
+        message: "Write a detailed description of your project",
       },
       {
-        type: 'input',
-        name: 'last_name',
-        messsage: "What's your last name",
-        default(){
-            return 'Doe';
+        type: 'list',
+        name: 'liscense',
+        message: 'What kind of liscense do you need?',
+        choices: ['Apache', 'Boost', 'BSD'],
+        filter(val) {
+          return val.toLowerCase();
         },
       },
+      
 
     ])
+
+    let readmeText = `# Project Description
+                      ${description}
+
+
+                      ## A second-level heading
+                      ${tableofcontents}
+
+
+                      ## A second-level heading
+                      ${installation}
+                      
+                      
+                      ## A second-level heading
+                      ${usage}
+
+
+                      ## A second-level heading
+
+                      ${generateLiscense(liscense)}
+                      
+                      
+                      ## A second-level heading
+
+                      ${generateLiscense(contributing)}
+                      
+                      
+                      ## A second-level heading
+
+                      
+                      ${generateLiscense(tests)}
+                      
+                      
+                      ## A second-level heading
+
+                      ${generateLiscense(questions)}
+
+                      ### A third-level heading
+                      
+                      
+                      `
+
+
+    fs.writeFile("README.md", readmeText)
+    console.log(description);
+
+    function generateLiscense(liscense){
+
+      if(liscense === "Apache"){
+
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+
+      }
+      else if(liscense === "Boost"){
+
+        return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+
+      }
+      else(liscense === "BSD"){ 
+
+        return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+
+      }
+
+          return``
+    }
